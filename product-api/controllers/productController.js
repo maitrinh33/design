290f9c486a40b8dd254424df46e7245eb8a02555
maitrinh33 <trinh.do220909@vnuk.edu.vn>
2024-11-26 20:00:56 +0700

    mt

--- product-api/controllers/productController.js
+++ product-api/controllers/productController.js
@@ -30,22 +30,32 @@ exports.createProduct = async (req, res) => {
   };
   
 
-// Cập nhật sản phẩm
+// Update product
 exports.updateProduct = async (req, res) => {
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
+    try {
+      // Check if a new image is uploaded, otherwise use the old image
+      const updatedProductData = {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.file ? req.file.path : req.body.image, // Keep the old image if no new file
+      };
+  
+      const updatedProduct = await Product.findByIdAndUpdate(req.params.id, updatedProductData, { new: true });
+      res.status(200).json(updatedProduct);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+  
 
-// Xóa sản phẩm
+// Delete product
 exports.deleteProduct = async (req, res) => {
-  try {
-    await Product.findByIdAndRemove(req.params.id);
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+    try {
+      await Product.findByIdAndRemove(req.params.id);
+      res.status(204).send(); // Successfully deleted
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
